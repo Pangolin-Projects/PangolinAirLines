6789132b40d7f76a81519e889567b6fa4f7112a3
Victor Augusto Martinez <fredericowrld@gmail.com>
2024-08-21 19:53:15 -0300

    Creating organizations and flights models

---
+++ PangolimAirLines.Api/Models/Flights.cs
@@ -0,0 +1,12 @@
+﻿namespace PangolimAirLines.Api.Models
+{
+    public class Flights
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string AirCraft { get; set; }
+
+        public int AvailableSits { get; set; }
+        public string TakeOff { get; set; }
+        public string Landing { get; set; }
+    }
+}
---
+++ PangolimAirLines.Api/Models/Organizations.cs
@@ -0,0 +1,9 @@
+﻿namespace PangolimAirLines.Api.Models
+{
+    public class Organizations
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
